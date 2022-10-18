6906e1e28cf0f91f40efa08b1e6983dd99b07442
CRADEL-5 <peteresosuakpo@gmail.com>
2022-10-18 13:23:19 -0700

    tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0
+	int count = 0;
+	char ch;
 
-	while (count++; count <= 9)
+		while (count++; count <= 9)
 	{
-		char ch
 			for  (ch = 'a'; ch <= 'z'; ch++)
 				_putchar(ch);
 		_putchar('\n');
