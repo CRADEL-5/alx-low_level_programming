df65db067894c3dbad0607a21afa868286d7eda6
CRADEL-5 <peteresosuakpo@gmail.com>
2022-10-18 13:10:15 -0700

    tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, on a line.
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0
+
+	while (count++; count <= 9)
+	{
+		char ch
+			for  (ch = 'a'; ch <= 'z'; ch++)
+				_putchar(ch);
+		_putchar('\n');
+	}
+}
