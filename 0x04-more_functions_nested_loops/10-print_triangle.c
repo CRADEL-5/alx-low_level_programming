fc8aba2fca87d9629b7dc170984aba3d6f7fe4d6
CRADEL-5 <peteresosuakpo@gmail.com>
2022-10-21 02:02:16 -0700

    kkaka

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of triangle
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int co, ro, sp;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 0; co <= (size - 1); co++)
+		{
+		for (sp = 0; sp < (size - 1) - ro; sp++)
+			{
+			_putchar(' ');
+			}
+		for (ro = 0; ro <= co; ro++)
+			{
+			_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
